abfce1f1a1c29983fb258f0503fd1075812c4816
Arun Sebastian <arunsebastian@gmail.com>
2024-02-14 17:09:57 +1100

    utils added for general purpose functions, added a seperate class for mobile view

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app/app';
+import { isMobile } from './utils/device';
 
 // Say something
 console.log('[ERWT] : Renderer execution started');
 
 // Application to Render
 const app = <App />;
+const target = document.getElementById('app-mobile');
 
+if (isMobile()) {
+    target.classList.add('mobile');
+}
 // Render application in DOM
-createRoot(document.getElementById('app')).render(app);
+createRoot(target).render(app);
