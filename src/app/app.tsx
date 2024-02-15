b878c750528dda752c101bf17e10d0436fe474cc
Arun Sebastian <arunsebastian@gmail.com>
2024-02-15 16:14:21 +1100

    toolbar group component added for test

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,14 @@
-//  import React, { useEffect, useState } from 'react';
-// import { icons } from './icons';
 import React from 'react';
+import ToolbarGroup from '@src/components/ToolbarGroup/toolbar-group';
 
 import './app.scss';
 
 const App: React.FC = () => {
-    return <div className='app-content'></div>;
+    return (
+        <div className='app-content'>
+            <ToolbarGroup />
+        </div>
+    );
 };
 
 export default App;
---
+++ src/components/Toolbar/toolbar.tsx
@@ -0,0 +1,11 @@
+//  import React, { useEffect, useState } from 'react';
+// import { icons } from './icons';
+import React from 'react';
+
+import './toolbar.scss';
+
+const App: React.FC = () => {
+    return <div className='app-content'></div>;
+};
+
+export default App;
