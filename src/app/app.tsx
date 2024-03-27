d9bb7c681726829a436ef2ac5c64885443ef1e89
Arun Sebastian <arunsebastian@gmail.com>
2024-03-27 15:33:24 +1000

    Logical glitch in load when only 3d view is available fixed

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import MapView from '../components/map-view/map-view';
 import Loader from '../components/loader/loader';
+
+import { useAppContext } from '@src/contexts/app-context-provider';
 import { MODE } from '@src/utils/constants';
+
 import './app.scss';
 
 const App: React.FC = () => {
+    const { mapView, sceneView, setActiveView } = useAppContext();
+
+    useLayoutEffect(() => {
+        if (!mapView && sceneView) {
+            // if there is only sceneView in the app
+            setActiveView(MODE.SCENE_VIEW);
+        } else if (mapView && !sceneView) {
+            // if there is only mapView in the app
+            setActiveView(MODE.MAP_VIEW);
+        }
+    }, [mapView, sceneView]);
+
     return (
         <div className='app-content'>
             <div className='app-view'>
